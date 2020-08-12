cfe9e6edb8a8fed2ab1bbc93a5ed08ffbaa73ac9
Guilherme <gui.mergulhao55@outlook.com>
2020-08-12 15:41:18 -0300

    Waifus RecyclerView Line Layout

---
+++ app/src/main/java/com/example/newkotlinproject/ui/view/WaifusListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newkotlinproject.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newkotlinproject.R
+
+class WaifusListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_waifus_list)
+    }
+}
